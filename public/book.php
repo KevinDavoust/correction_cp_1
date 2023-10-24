0fa7d266a2d721c22a558ea6a2e46ffd92cfe5b8
KevinDavoust <kevin.davoust@orange.fr>
2023-10-24 15:40:29 +0200

    correction en live

--- public/book.php
+++ public/book.php
@@ -1,3 +1,46 @@
+<?php
+require_once "../connec.php";
+$pdo = new PDO(DSN, USER, PASS);
+
+$errors = [];
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (!isset($_POST['name'])) {
+        $errors[] = 'Name is empty';
+    }
+
+    if (!isset($_POST['payment']) || $_POST['payment'] <= 0) {
+        $errors[] = 'Payment must be higher than 0';
+    }
+
+    if (count($errors) > 0) {
+        var_dump($errors);
+        die();
+    }
+
+    $name = trim($_POST['name']);
+    $payment = trim($_POST['payment']);
+
+    $query = "INSERT INTO bribe(name, payment) VALUES (:name, :payment)";
+    $statement = $pdo->prepare($query);
+    $statement->bindValue(':name', $name, PDO::PARAM_STR);
+    $statement->bindValue(':payment', $payment, PDO::PARAM_INT);
+    $statement->execute();
+}
+
+$query = 'SELECT * FROM bribe ORDER BY name';
+$statement = $pdo->query($query);
+$bribes = $statement->fetchAll();
+
+$query = 'SELECT SUM(payment) FROM bribe';
+$total = $pdo->query($query)->fetch();
+
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -21,10 +64,35 @@
             <div class="page leftpage">
                 Add a bribe
                 <!-- TODO : Form -->
+
+                <form method="post">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name">
+                    <label for="">Payment</label>
+                    <input type="text" id="payment" name="payment">
+                    <button type="submit">Submit</button>
+                </form>
             </div>
 
             <div class="page rightpage">
                 <!-- TODO : Display bribes and total paiement -->
+                <table>
+
+                    <?php
+                    foreach ($bribes as $bribe) {
+                        ?>
+                        <tr>
+                            <td><?= $bribe['name'] ?></td>
+                            <td><?=  $bribe['payment'] ?></td>
+                        </tr>
+
+                        <?php } ?>
+
+                    <tr>
+                        <td>Total :</td>
+                        <td> <?= $total[0] ?> </td>
+                    </tr>
+                </table>
             </div>
         </div>
         <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
